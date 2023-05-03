0d5e54c8aa1fbf5526779e5686fc0456f38184f0
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-03 18:16:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,22 @@
-import requests
+import openai
 import streamlit as st
-from bs4 import BeautifulSoup
 
-url = "https://www.google.com/"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
-}
+# 用你自己的API密钥替换“your_api_key”
+openai.api_key = "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"
 
+# 创建一个文本生成请求
+response = openai.Completion.create(
+  engine="text-davinci-002",  # 选择你想要使用的引擎
+  prompt="什么是人工智能?",  # 输入文本提示
+  max_tokens=100,  # 生成文本的最大长度
+  n=1,  # 生成文本的数量
+  stop=None,  # 设置停止生成文本的标记
+  temperature=0.7,  # 控制生成文本的创造性（较高的值会导致更具创意的生成，较低的值会导致更保守的生成）
+)
 
-try:
-    response=requests.get(url,headers=headers)
-    soup = BeautifulSoup(response.text, "html.parser")
-    # 在此处添加您要提取的信息
-    # 例如，提取所有段落或其他元素
-    # paragraphs = soup.find_all("p")
-    st.write(soup.prettify())
-except:
-    st.write('访问失败')
+# 从响应中获取生成的文本
+generated_text = response.choices[0].text.strip()
+
+# 打印生成的文本
+st.write(generated_text)
+
