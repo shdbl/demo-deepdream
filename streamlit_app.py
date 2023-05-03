0ea2280b5542c9bac25a0e6d65ae61cff9b96642
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-03 18:28:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ openai.api_key = "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"
 
 # 创建一个文本生成请求
 response = openai.Completion.create(
+  model="gpt-3.5-turbo",
   prompt="Create a Python function to add two numbers:",
   temperature=0.5,
   max_tokens=50,
