51cc4e014eb049eaad3848a7212cdc197eadce2b
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-03 18:24:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,13 @@ openai.api_key = "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"
 
 # 创建一个文本生成请求
 response = openai.Completion.create(
-  engine="text-davinci-002",  # 选择你想要使用的引擎
-  prompt="什么是人工智能?",  # 输入文本提示
-  max_tokens=100,  # 生成文本的最大长度
-  n=1,  # 生成文本的数量
-  stop=None,  # 设置停止生成文本的标记
-  temperature=0.7,  # 控制生成文本的创造性（较高的值会导致更具创意的生成，较低的值会导致更保守的生成）
+  engine="davinci-codex",
+  prompt="Create a Python function to add two numbers:",
+  temperature=0.5,
+  max_tokens=50,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
 )
 
 # 从响应中获取生成的文本
