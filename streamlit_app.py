53cb850526b73c7fe1ac702d77fdcfc52e329e19
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-03 18:32:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,6 @@
 import openai
 import streamlit as st
 
-# 用你自己的API密钥替换“your_api_key”
-openai.api_key = "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"
 
-# 创建一个文本生成请求
-response = openai.Completion.create(
-  model="gpt-3.5-turbo",
-  prompt="Create a Python function to add two numbers:",
-  temperature=0.5,
-  max_tokens=50,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-)
-
-# 从响应中获取生成的文本
-generated_text = response.choices[0].text.strip()
-
-# 打印生成的文本
-st.write(generated_text)
+st.write('hello')
 
