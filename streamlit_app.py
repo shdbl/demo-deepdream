72f2fe688333dba48a32ca229e1f42fb93d7fa3a
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-03 18:26:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ openai.api_key = "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"
 
 # 创建一个文本生成请求
 response = openai.Completion.create(
-  engine="davinci-codex",
   prompt="Create a Python function to add two numbers:",
   temperature=0.5,
   max_tokens=50,
