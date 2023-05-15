b76c7672e736596fc6d2f94d04d66986be32d748
shdbl <102776480+shdbl@users.noreply.github.com>
2023-05-15 18:17:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,50 @@ import streamlit as st
 import random
 import time
 
+# 统计抽奖次数和距离保底的次数
+cjcs=0
+xbd=30
+with open('./redeem_code.txt','r') as file:
+    for line in file.readlines():
+        if line.strip().endswith('#'):
+            cjcs+=1
+            xbd-=1
+        elif line.strip().endswith('%'):
+            xbd=30
+
+
 # 定义函数，用于验证密码是否正确
 def check_and_update_code(input_code):
     lines=[]
     found=False
-
     with open('./redeem_code.txt','r') as file:
         for line in file:
             line=line.strip()
             if line==input_code:
                 found=True
-                lines.append(f"#{line}")
+                lines.append(f"{line}#")
             else:
                 lines.append(line)
-
     if found:
         with open('./redeem_code.txt','w') as file:
             file.write("\n".join(lines))
 
     return found
+
+# 定标记抽中保底
+def sign_baodi(input_code):
+    lines=[]
+    with open('D:/Python/玩/抽奖/redeem_code.txt','r') as file:
+        for line in file:
+            line=line.strip()
+            if line==input_code+'#':
+                lines.append(f"{line}%")
+            else:
+                lines.append(line)
+
+        with open('./redeem_code.txt','w') as file:
+            file.write("\n".join(lines))
+
 
 
 # 定义奖项及其对应的权重
@@ -47,6 +72,8 @@ def lucky_draw():
             return prize
 
 # 在页面上显示密码输入框
+st.header('已抽次数：{}'.format(cjcs))
+st.subheader(f'距离保底{xbd}')
 pw = st.text_input("请输入兑换码")
 # 如果密码输入框不为空
 if pw:
@@ -54,10 +81,13 @@ if pw:
     if check_and_update_code(pw):
         # 在页面上显示抽奖按钮
         result = lucky_draw()
+        if result=='一等奖':
+            sign_baodi(pw)
         # 在页面上显示抽奖结果
         st.balloons()
         time.sleep(1)
-        st.success("恭喜您，抽中了{}！".format(result))
+        st.success("抽中了{}！".format(result))
+
     else:
         # 在页面上显示密码错误提示
         st.write("兑换码错误，请重新输入！")
